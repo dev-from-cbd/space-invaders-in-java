eadedd2d5f9fd3470d26e3672f3f63434904f67f
dev-from-cbd <veryfatwombat@gmail.com>
2024-09-18 06:30:34 +0930

    Game

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //window variables
+        int tileSize = 32;
+        int rows = 16;
+        int columns = 16;
+        int boardWidth = tileSize * columns; //32*16 = 512px
+        int boatdHeight = tileSize * rows; //32*16 = 512px
+
+        JFrame frame = new JFrame("Space Invaders in Java");
+        frame.setVisible(true);
     }
 }
