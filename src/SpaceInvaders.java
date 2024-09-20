1d7894bfb087eee0e4f1ef96cfd67ab4d4b55699
dev-from-cbd <veryfatwombat@gmail.com>
2024-09-21 08:47:52 +0930

    Game

--- src/SpaceInvaders.java
+++ src/SpaceInvaders.java
@@ -2,7 +2,18 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
+import javax.swing.*;
 
-public class SpaceInvaders {
+public class SpaceInvaders extends JPanel {
+    //board
+    int tileSize = 32;
+    int rows = 16;
+    int columns = 16;
+    int boardWidth = tileSize * columns; //32*16 = 512px
+    int boatdHeight = tileSize * rows; //32*16 = 512px
 
+    SpaceInvaders() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.BLACK);
+    }
 }
