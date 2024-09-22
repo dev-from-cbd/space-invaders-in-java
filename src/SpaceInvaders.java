1bfa37d6b9541717c13e123d4b22da9c60278fd3
dev-from-cbd <veryfatwombat@gmail.com>
2024-09-23 06:49:55 +0930

    Game

--- src/SpaceInvaders.java
+++ src/SpaceInvaders.java
@@ -4,14 +4,25 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class SpaceInvaders extends JPanel {
+public class SpaceInvaders extends JPanel implements ActionListener {
     class Block {
         int x;
         int y;
         int width;
         int height;
+        Image img;
+        boolean alive = true; //used for aliens
+        boolean used = false; //used for bullets
+
+        Block(int x, int y, int width, int height, Image img) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.img = img;
+        }
     }
-    //board
+    //Board
     int tileSize = 32;
     int rows = 16;
     int columns = 16;
@@ -25,9 +36,19 @@ public class SpaceInvaders extends JPanel {
     Image alienYellowImg;
     ArrayList<Image> alienImgArray;
 
+    //Ship
+    int shipWidth = tileSize*2; //64px
+    int shipHeight = tileSize; //32px
+    int shipX = tileSize*columns/2 - tileSize;
+    int shipY = boardHeight - tileSize*2;
+
+    Block ship;
+
+    Timer gameLoop;
+
     SpaceInvaders() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.black);
+        setBackground(Color.BLACK);
 
         //Load Images
         shipImg = new ImageIcon(getClass().getResource("./ship.png")).getImage();
@@ -41,5 +62,23 @@ public class SpaceInvaders extends JPanel {
         alienImgArray.add(alienCyanImg);
         alienImgArray.add(alienMagentaImg);
         alienImgArray.add(alienYellowImg);
+
+        ship = new Block(shipX, shipY, shipWidth, shipHeight, shipImg);
+
+        //Game Timer
+        gameLoop = new Timer(1000/60, this); //1000/60 = 16.6
+        gameLoop.start();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        //Ship
+        g.drawImage(ship.img, ship.x, ship.y, ship.width, ship.height, null);
+
+        //Aliens
     }
 }
